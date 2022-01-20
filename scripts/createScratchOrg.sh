6233657cc5c7e670d7dc32c85472003b4299096a
rsosemann <robert@soesemann.com>
2022-01-20 15:46:34 +0100

    Updated few files from more recent repos

--- scripts/createScratchOrg.sh
+++ scripts/createScratchOrg.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+orglist:#!/bin/bash
 source `dirname $0`/config.sh
 
 execute() {
   $@ || exit
 }
 
-if [ -z "$secrets.DEV_HUB_URL" ]; then
+listif [ -z "$DEV_HUB_URL" ]; then
   echo "set default devhub user"
   execute sfdx force:config:set defaultdevhubusername=$DEV_HUB_ALIAS
 
@@ -13,29 +13,22 @@ if [ -z "$secrets.DEV_HUB_URL" ]; then
   sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS
 fi
 
+echo "Creating scratch org"
+execute sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f ./config/project-scratch-def.json -d 30
 
-echo "Creating scratch ORG"
-execute sfdx force:org:create -a $SCRATCH_ORG_ALIAS -s -f ./config/project-scratch-def.json -d 7
+echo "Install dependencies"
+#execute sfdx texei:package:dependencies:install -u $SCRATCH_ORG_ALIAS -v $DEV_HUB_ALIAS
+
+echo "Make sure Org user is english"
+sfdx force:data:record:update -s User -w "Name='User User'" -v "Languagelocalekey=en_US"
 
 echo "Pushing changes to scratch org"
 execute sfdx force:source:push
 
 echo "Assigning permission"
-# execute sfdx force:user:permset:assign -n Admin
+execute sfdx force:user:permset:assign -n Admin
 
-echo "Make sure Org user is english"
-sfdx force:data:record:update -s User -w "Name='User User'" -v "Languagelocalekey=en_US"
+echo "Create sample data"
+#sfdx force:apex:execute -f scripts/createSampleData.apex -u $SCRATCH_ORG_ALIAS
 
-echo "Running apex tests"
-execute sfdx force:apex:test:run -l RunLocalTests -w 30
-
-if [ -f "package.json" ]; then
-  echo "Running jest tests"
-  execute npm install
-  execute npm run test:unit
-fi
-
-if [ $secrets.DEV_HUB_URL ]; then
-  echo "deleting old scratch org"
-  sfdx force:org:delete -p -u $SCRATCH_ORG_ALIAS
-fi
+sh runTests.sh
